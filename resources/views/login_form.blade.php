cb1f1b166f3a89c03b6d2e19e01285b3650405aa
Fikri645 <fikriayam4@gmail.com>
2024-03-27 14:12:22 +0700

    templates blade

--- resources/views/login_form.blade.php
+++ resources/views/login_form.blade.php
@@ -1,18 +1,6 @@
-<?php
-session_start();
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
-</head>
-
-<body>
+@extends('templates')
+@section('title', 'Login')
+@section('content')
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-6">
@@ -32,14 +20,9 @@ session_start();
                             {{ session('message') }}
                         </div>
                     @endif
-                    <button class="btn btn-primary"">Login</button>
+                    <button class="btn btn-primary">Login</button>
                 </form>
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+@endsection
