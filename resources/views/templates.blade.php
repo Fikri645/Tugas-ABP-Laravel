029d1288af75c57498d7aa274775ab412d137684
Fikri645 <fikriayam4@gmail.com>
2024-03-27 14:15:35 +0700

    update templates

--- resources/views/templates.blade.php
+++ resources/views/templates.blade.php
@@ -13,6 +13,14 @@ session_start();
 </head>
 
 <body>
+    @auth
+    <div class="row justify-content-end mt-3">
+        <div class="col-md-3">
+            Welcome, {{ Auth::user()->name }}
+            <a class="btn btn-danger" href="/logout">Logout</a>
+        </div>
+    </div>
+    @endauth
     @yield('content')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
